9e40fce2a7a3c6d75f72b97e6585aa0c39c92293
Jheeseong <gmltjd6801@gmail.com>
2022-04-21 17:20:13 +0900

    algorithm

--- src/dp/tile_2xn.java
+++ src/dp/tile_2xn.java
@@ -6,30 +6,31 @@ import java.io.InputStreamReader;
 
 public class tile_2xn {
 
-    static int[] dp;
+    static Long[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        dp = new int[n + 1];
+        dp = new Long[1001];
 
         for (int i = 0; i < n + 1; i++) {
-            dp[i] = -1;
+            dp[i] = null;
         }
 
-        dp[1] = 1;
-        dp[2] = 2;
+        dp[1] = 1L;
+        dp[2] = 2L;
         System.out.println(fibonacci(n));
         br.close();
 
 
     }
 
-    static int fibonacci(int N) {
-        if (dp[N] == -1) {
-            dp[N] = fibonacci(N - 2) + fibonacci(N - 1) % 10007;
+    static Long fibonacci(int N) {
+        if (dp[N] == null) {
+            dp[N] = fibonacci(N - 2) + fibonacci(N - 1);
+            dp[N] %= 10007;
         }
         return dp[N];
     }
