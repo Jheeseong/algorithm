9e10bc8db44024ce7592812bf38e5b9ad20184b2
Jheeseong <gmltjd6801@gmail.com>
2022-04-24 22:15:04 +0900

    algorithm

---
+++ src/dp/boj_9465.java
@@ -0,0 +1,38 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_9465 {
+    static int dp[][];
+    static int cost[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int n = Integer.parseInt(br.readLine());
+            dp = new int[2][n + 1];
+            cost = new int[2][n + 1];
+            for (int j = 0; j < 2; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int k = 1; k <= n; k++) {
+                    cost[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+            System.out.println(recur(n));
+        }
+    }
+
+    static int recur(int n) {
+        dp[0][1] = cost[0][1];
+        dp[1][1]= cost[1][1];
+        for (int j = 2; j <= n; j++) {
+            dp[0][j] = Math.max(dp[1][j - 1], dp[1][j - 2]) + cost[0][j];
+            dp[1][j] = Math.max(dp[0][j - 1], dp[0][j - 2]) + cost[1][j];
+        }
+        return Math.max(dp[0][n], dp[1][n]);
+    }
+}
