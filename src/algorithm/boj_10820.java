aa1bc6f8c15610768b474d5167ecb85b63b11efa
Jheeseong <gmltjd6801@gmail.com>
2022-05-08 21:03:36 +0900

    algorithm

--- src/algorithm/boj_10820.java
+++ src/algorithm/boj_10820.java
@@ -3,14 +3,14 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class boj_10820 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        String str = "";
-        while ((str = br.readLine()) != null) {
+        StringBuilder sb;
+        String str;
+        while ((str = br.readLine()) != null){
+            sb =new StringBuilder();
             int arr[] = new int[4];
             for (int j = 0; j < str.length(); j++) {
                 if ((int) str.charAt(j) >= 97) {
@@ -19,13 +19,15 @@ public class boj_10820 {
                     arr[1] += 1;
                 } else if (48 <= (int) str.charAt(j) && (int) str.charAt(j) < 65) {
                     arr[2] += 1;
-                } else if ((int) str.charAt(j) == 32){
+                } else if ((int) str.charAt(j) == 32) {
                     arr[3] += 1;
                 }
             }
-            sb.append(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3]).append('\n');
-        }
+
+            for (int i : arr) {
+                sb.append(i).append(" ");
+            }
             System.out.println(sb);
-        br.close();
+        }
     }
 }
