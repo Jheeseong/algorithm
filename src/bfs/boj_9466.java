0884d6d81da9617a10a1cc5ea49387bfff234e90
Jheeseong <gmltjd6801@gmail.com>
2022-05-19 03:46:23 +0900

    algorithm

--- src/bfs/boj_9466.java
+++ src/bfs/boj_9466.java
@@ -3,65 +3,50 @@ package bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class boj_9466 {
-    static ArrayList<ArrayList<Integer>> check;
-    static boolean[] checked;
-    static int N;
-
+    static int[] arr;
+    static boolean[] visit,done;
+    static int cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int T = Integer.parseInt(br.readLine());
-        int res = 0;
 
         while (T-- > 0) {
-            N = Integer.parseInt(br.readLine());
-            int[] y = new int[N + 1];
-            check = new ArrayList<>();
+            int N = Integer.parseInt(br.readLine());
+
+            arr = new int[N + 1];
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i <= N; i++) {
-                check.add(new ArrayList<>());
-            }
             for (int i = 1; i <= N; i++) {
-                int x = i;
-                y[i] = Integer.parseInt(st.nextToken());
-
-                check.get(y[i]).add(x);
-                check.get(x).add(y[i]);
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-
-            checked = new boolean[N + 1];
+            visit = new boolean[N + 1];
+            done = new boolean[N + 1];
+            cnt = 0;
             for (int i = 1; i <= N; i++) {
-                if (!checked[i]) {
-                    res = bfs(i);
+                if (!done[i]) {
+                    explore(i);
                 }
             }
+            System.out.println(N - cnt);
         }
-        System.out.println(res);
     }
 
-    static public int bfs(int start) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(start);
-        checked[start] = true;
-        int cnt = 0;
+    public static void explore(int start) {
+        visit[start] = true;
 
-        while (!queue.isEmpty()) {
-            int temp = queue.poll();
-
-            for (int i = 0; i < check.get(temp).size(); i++) {
-                if (!checked[check.get(temp).get(i)]) {
-                    checked[check.get(temp).get(i)] = true;
-                    queue.offer(check.get(temp).get(i));
-                    cnt ++;
+        if (!visit[arr[start]]) {
+            explore(arr[start]);
+        } else {
+            if (!done[arr[start]]) {
+                cnt++;
+                while (arr[start] != start) {
+                    cnt++;
+                    arr[start] = arr[arr[start]];
                 }
             }
         }
-        return cnt;
+        done[start] = true;
     }
 }
