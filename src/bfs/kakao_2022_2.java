775bf99702ec41e585afe0baf1705d817b9c87e2
Jheeseong <gmltjd6801@gmail.com>
2022-05-19 22:15:10 +0900

    algorithm

--- src/bfs/kakao_2022_2.java
+++ src/bfs/kakao_2022_2.java
@@ -1,11 +1,5 @@
 package bfs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class kakao_2022_2 {
     public static void main(String[] args) {
