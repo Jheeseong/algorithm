7acec67f3c233d344705cc4cbf93b4190dedd395
Jheeseong <gmltjd6801@gmail.com>
2022-05-24 23:13:20 +0900

    algorithm

---
+++ src/algorithm/samsung_sw_2.java
@@ -0,0 +1,51 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class samsung_sw_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            String s = "";
+            String s2 = "";
+            ArrayList<Integer> list = new ArrayList<>();
+            s = br.readLine();
+            for (int j = 0; j < s.length(); j++) {
+                list.add((int) s.charAt(j) - '0');
+            }
+            int a = 2;
+            int cnt = 0;
+            while (true) {
+                s2 = String.valueOf(Integer.valueOf(s) * a);
+                if (s2.length() > list.size()) {
+                    break;
+                }
+                if (list.get(0) == 0) {
+                    break;
+                }
+                for (int j = 0; j < s.length(); j++) {
+                    for (int k = 0; k < list.size(); k++) {
+                        if ((s2.charAt(j) - '0') == list.get(k)) {
+                            cnt++;
+                        }
+                    }
+                }
+                if (cnt == s.length()) {
+                    System.out.println("#" + i + " possible");
+                    break;
+                } else {
+                    a++;
+                    cnt = 0;
+                }
+            }
+            if (cnt != list.size()) {
+                System.out.println("#" + i + " impossible");
+            }
+        }
+    }
+}
