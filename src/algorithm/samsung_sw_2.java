3543311e93524bc6da4101f1f8182eae0e532784
Jheeseong <gmltjd6801@gmail.com>
2022-05-25 17:37:26 +0900

    algorithm

--- src/algorithm/samsung_sw_2.java
+++ src/algorithm/samsung_sw_2.java
@@ -3,7 +3,7 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class samsung_sw_2 {
     public static void main(String[] args) throws IOException {
@@ -11,48 +11,39 @@ public class samsung_sw_2 {
 
         int T = Integer.parseInt(br.readLine());
         for (int i = 1; i <= T; i++) {
-            String s = "";
-            String s2 = "";
-            ArrayList<Integer> list = new ArrayList<>();
-            s = br.readLine();
-            int arr[] = new int[s.length()];
+            String s = br.readLine();
+            int n = Integer.parseInt(s);
+            int[] check = new int[9];
+            int[] num1 = new int[10];
+            int[] num2;
+
             for (int j = 0; j < s.length(); j++) {
-                list.add((int) s.charAt(j) - '0');
+                num1[Integer.parseInt(s.substring(j, j + 1))]++;
             }
-            int a = 2;
-            int cnt = 0;
-            while (true) {
-                s2 = String.valueOf(Integer.valueOf(s) * a);
-                if (s2.length() > list.size()) {
-                    break;
-                }
-                if (list.get(0) == 0) {
-                    break;
-                }
-                for (int j = 0; j < list.size(); j++) {
-                    for (int k = 0; k < list.size(); k++) {
-                        if ((s2.charAt(j) - '0') == list.get(k)) {
-                            arr[j] += 1;
-                        }
-                    }
-                }
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] == 1) {
-                        cnt++;
-                    }
-                }
-                if (cnt == list.size()) {
-                    System.out.println("#" + i + " possible");
-                    break;
-                } else {
+
+            int a= 2;
+            for (int j = 0; j < 9; j++) {
+                if (String.valueOf(n * a).length() == s.length()) {
+                    check[j] = n * a;
                     a++;
-                    cnt = 0;
-                    arr = new int[list.size()];
+                } else {
+                    check[j] = 0;
+                    break;
                 }
             }
-            if (cnt != list.size()) {
-                System.out.println("#" + i + " impossible");
+
+            boolean t = false;
+            for (int j = 0; j < 9; j++) {
+                num2 = new int[10];
+                for (int k = 0; k < String.valueOf(check[j]).length(); k++) {
+                    num2[Integer.parseInt(String.valueOf(check[j]).substring(k, k + 1))]++;
+                }
+                if (Arrays.equals(num1, num2)) {
+                    t = true;
+                    break;
+                }
             }
+            System.out.printf("#%d %s\n", i , t == true ? "possible" : "impossible");
         }
     }
 }
