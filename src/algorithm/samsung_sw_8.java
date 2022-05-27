8e8c1ac982edf6d57d89d2dfa74ebdb41b94d854
Jheeseong <gmltjd6801@gmail.com>
2022-05-27 23:32:55 +0900

    algorithm

---
+++ src/algorithm/samsung_sw_8.java
@@ -0,0 +1,56 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class samsung_sw_8 {
+    static int[] done;
+    static int N;
+    static int cnt;
+    public static void main(String args[]) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tn = 1; tn <= T; tn++) {
+            N = Integer.parseInt(br.readLine());
+            cnt = 0;
+
+            done = new int[N];
+
+            check(0);
+            System.out.println(cnt);
+        }
+    }
+
+    public static void check(int depth) {
+        if (depth == N) {
+            cnt++;
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            done[depth] = i;
+
+            if (Possibility(depth)) {
+                check(depth + 1);
+            }
+        }
+    }
+
+    public static boolean Possibility(int col) {
+
+        for (int i = 0; i < col; i++) {
+            if (done[col] == done[i]) {
+                return false;
+            } else if (Math.abs(col - i) == Math.abs(done[col] - done[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
