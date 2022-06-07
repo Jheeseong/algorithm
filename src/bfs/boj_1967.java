2c9830f35ea8e0423588c20bc489c3bbefa1a681
Jheeseong <gmltjd6801@gmail.com>
2022-06-07 17:53:09 +0900

    algorithm

--- src/bfs/boj_1967.java
+++ src/bfs/boj_1967.java
@@ -4,13 +4,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class boj_1967 {
-    static ArrayList<Node> list[] ;
+    static ArrayList<Node> list[];
     static int n;
     static int max = 0;
     static boolean visited[];
     static int max_idx = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -18,64 +20,57 @@ public class boj_1967 {
         n = Integer.parseInt(br.readLine());
 
 
-        list = new ArrayList[n+1];
+        list = new ArrayList[n + 1];
 
-        for(int i=0; i<=n; i++) {
+        for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
         }
 
 
-
-
-        for(int i=0; i<n-1; i++) {
-            String [] t = br.readLine().split(" ");
-            int parent = Integer.parseInt(t[0]);
-            int child = Integer.parseInt(t[1]);
-            int weight = Integer.parseInt(t[2]);
-            list[parent].add(new Node(child,weight));
-            list[child].add(new Node(parent,weight));
+        for (int i = 0; i < n - 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int parent = Integer.parseInt(st.nextToken());
+            int child = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list[parent].add(new Node(child, weight));
+            list[child].add(new Node(parent, weight));
         }
 
-        visited = new boolean[n+1];
+        visited = new boolean[n + 1];
         visited[1] = true;
-        dfs(1,0);
+        dfs(1, 0);
 
 
-        visited = new boolean[n+1];
+        visited = new boolean[n + 1];
         visited[max_idx] = true;
-        dfs(max_idx,0);
+        dfs(max_idx, 0);
         System.out.println(max);
 
     }
+
     public static void dfs(int idx, int cnt) {
 
-        if(max < cnt) {
+        if (max < cnt) {
             max = cnt;
             max_idx = idx;
         }
 
 
-
-        for(Node a : list[idx]) {
-            if(!visited[a.idx]) {
+        for (Node a : list[idx]) {
+            if (!visited[a.idx]) {
                 visited[a.idx] = true;
-                dfs(a.idx, cnt+a.cnt);
+                dfs(a.idx, cnt + a.cnt);
             }
         }
 
 
-
-
-
-
-
-
     }
-}
-class Node{
-    int idx,cnt;
-    Node(int idx, int cnt){
-        this.idx = idx;
-        this.cnt = cnt;
+    public static class Node {
+        int idx, cnt;
+
+        Node(int idx, int cnt) {
+            this.idx = idx;
+            this.cnt = cnt;
+        }
     }
 }
