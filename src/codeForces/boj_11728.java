80e0fb8ad2a0f829a5758236f4478db0461caebe
Jheeseong <gmltjd6801@gmail.com>
2022-06-11 22:24:49 +0900

    algorithm

--- src/codeForces/boj_11728.java
+++ src/codeForces/boj_11728.java
@@ -2,34 +2,34 @@ package codeForces;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class boj_11728 {
     public static void main(String[] args) throws IOException, IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] input=br.readLine().split(" "); // 첫 번째 개행 읽기
-        int n=Integer.parseInt(input[0]), m=Integer.parseInt(input[1]);
-        int size=n+m;
-        int[] result=new int[size];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int size = n + m;
+        int[] result = new int[size];
 
-        String[] arrayA=br.readLine().split(" "); // 두 번째 줄 읽기
-        String[] arrayB=br.readLine().split(" "); // 세 번째 줄 읽기
-
-        int idx=0;
-        for(int i=0;i<n;i++) { // 배열 A 넣기
-            result[idx++]=Integer.parseInt(arrayA[i]);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) { // 배열 A 넣기
+            result[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=0;i<m;i++) { // 배열 B 넣기
-            result[idx++]=Integer.parseInt(arrayB[i]);
+        st = new StringTokenizer(br.readLine());
+        for (int i = n; i < n + m; i++) { // 배열 B 넣기
+            result[i] = Integer.parseInt(st.nextToken());
+
         }
         Arrays.sort(result); // 오름차순 정렬
 
+        StringBuilder sb = new StringBuilder();
         for(int r:result) {
-            bw.write(String.valueOf(r)+" ");
+            sb.append(r).append(" ");
         }
 
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
